c21a1a9d972c898fc8d99125373217d2758c9dcd
JostinCG <ma@qtimporta.com>
2024-09-18 21:09:00 -0600

    Actual Desarrollo

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,8 +1,36 @@
+import LoginButton from './login/LoginButton';
+import Cart from './components/Cart/Cart';
+import Form from './form/Form';
+// import Counter from './components/counter/Counter';
+
+// import './App.css';
+
 function App() {
   return (
-    <main>
-      <div>Hello!</div>
-    </main>
+    <div className="App">
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <LoginButton />
+      </header> */}
+      <main>
+
+        {/* <div className='container'>
+          <h2>Counter</h2>
+          <Counter />
+        </div> */}
+
+        <div className='container'>
+          <h2>Cart</h2>
+          <Cart />
+        </div>
+
+        <div className='container'>
+          <h2>Form</h2>
+          <Form />
+        </div>
+
+      </main>
+    </div>
   );
 }
 
--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import Cart from "./components/Cart/Cart";
+import Form from "./form/Form";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
